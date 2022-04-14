d8217e7e40f53cf3ab08b9c68d67ed93a4ef7bd0
Simon Green <simon@captaincodeman.com>
2022-04-14 13:08:15 -0600

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto"
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +9,6 @@ const config = {
 
   kit: {
     adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
   },
 };
 
